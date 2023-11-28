d955578950a3d8f8e14d025862c0b7cfd2c2e335
Your Token <your-token@example.com>
2023-11-28 10:54:25 +0200

    update data in system :)

--- server/updateData.php
+++ server/updateData.php
@@ -1,6 +1,29 @@
 <?php
-$ip = "127.0.0.1:161";
+$sysContact= $_POST['sysContact'];
+$sysName= $_POST['sysName'];
+$sysLocation= $_POST['sysLocation'];
 
-snmp2_set($ip, "publicAll", '.1.3.6.1.2.1.1.6', 's', '0 1 2 3 4');
+$ip = "127.0.0.1:161";
+$community = 'network2';  //ana hek smetha
+//snmp2_set($ip, "publicAll", '.1.3.6.1.2.1.1.6', 's', '0 1 2 3 4');
 // '.1.3.6.1.2.1.1.6' or '.1.3.6.1.2.1.1.6.0'
 // ❤️مش عارف مين الصح جربي الجهتين احلى شريكة وردة الوردات
+
+
+// sysContact
+if(!empty($sysContact)){
+snmp2_set($ip, $community, '.1.3.6.1.2.1.1.4.0', 's', $sysContact);
+}
+//sysName
+if(!empty($sysName)){
+snmp2_set($ip, $community,'.1.3.6.1.2.1.1.5.0' , 's', $sysName);
+}
+//sysLocation
+if(!empty($sysLocation)){
+snmp2_set($ip, $community,'.1.3.6.1.2.1.1.6.0' , 's', $sysLocation);
+}
+echo "<script> 
+  document.location.href = '../index.html'
+  </script>";
+
+?>
